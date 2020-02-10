fda8bdeb3463da53ba4084f23f66880e4dead11f
cuifangyu <fangyu.cui0109@gmail.com>
2020-02-09 16:49:49 -0800

    week-5 assignment

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import SlugBotton from './SlugButton';
+import './ArticleListItem.module.css';
 
 const ArticleListItem = (props) => {
     return (
@@ -9,8 +11,8 @@ const ArticleListItem = (props) => {
             <body>
                 <h1>{props.article.title}</h1>
                 <p>{props.article.shortText}</p>
-                <time datetime={props.article.pubYear}>{props.article.pubDate}</time>
-                <button onClick = {() => (alert(props.article.slug))}>show article slug</button> 
+                <time dateTime = {props.article.pubYear}>{props.article.pubDate}</time>
+                <SlugBotton slug = {props.article.slug} buttonText = {props.article.author}></SlugBotton>
             </body>
         </div>
     );
